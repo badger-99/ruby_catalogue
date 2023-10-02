1b51fabfc01d9a871866c5b809e0427f00cc2fb7
devendra-alt <devendramulewa171@gmail.com>
2023-10-02 23:22:28 +0530

    Add operation methods in app.rb

--- lib/app.rb
+++ lib/app.rb
@@ -1,5 +1,42 @@
 class App
-  def list_book
-    puts 'hello ruby!'
+
+  def add_book
+    # ** add book logic
+  end
+
+  def add_music_album
+    # ** add music album logic
+  end
+
+  def add_game
+    # ** add game logic
+  end
+
+  def list_books
+    # ** list books logic
+  end
+
+  def list_music_albums
+    # TODO list music albums
+  end
+
+  def list_games
+    # TODO list games
+  end
+
+  def list_geners
+    # TODO list geners
+  end
+
+  def list_labels
+    # TODO list labels
+  end
+
+  def list_authors
+    # TODO list authors
+  end
+
+  def list_books
+    # TODO list books
   end
 end
