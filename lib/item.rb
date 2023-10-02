c96208f2618fb0df07ebd586bc13991a7c3e37bc
Haider <malikhaiderkhan81@gmail.com>
2023-10-02 20:32:58 +0500

    fix offenses

--- lib/item.rb
+++ lib/item.rb
@@ -1,47 +1,37 @@
 class Item
   attr_accessor :publish_date
-  attr_reader: :id, :genre, :author, :label, :archived
+  attr_reader :id, :genre, :author, :label, :archived
 
   def initialize(genre, author, label, date, archived: false)
     @id = Random.rand(1..1000)
-    set_genre(genre)
-    set_author(author)
-    set_label(label)
+    add_genre(genre)
+    add_author(author)
+    add_label(label)
     @publish_date = Date.git parse_date(date)
     @archived = archived
   end
 
-  def set_genre(genre)
+  def add_genre(genre)
     @genre = genre
     genre.array.push(self) unless genre.array.include?(self)
-    end
   end
 
-  def set_author(author)
+  def add_author(author)
     @author = author
     author.array.push(self) unless author.array.include?(self)
-    end
   end
 
-  def set_label(label)
+  def add_label(label)
     @label = label
     label.array.push(self) unless label.array.include?(self)
-    end
   end
 
   def can_be_archived?
     current_date = Date.today
-    if current_date - @publish_date >= 10
-      true
-    else
-      false
+    current_date - @publish_date >= 10
   end
 
   def move_to_archive
-    if can_be_archived?
-      @archived = true
-    else
-      @archived = false
+    @archived = can_be_archived?
   end
-
 end
