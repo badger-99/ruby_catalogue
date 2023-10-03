3cb99eb615beb80324f5b995dd9243b349beb188
Haider <malikhaiderkhan81@gmail.com>
2023-10-03 17:15:12 +0500

    fix offenses

--- lib/book.rb
+++ lib/book.rb
@@ -1,14 +1,14 @@
 require_relative 'item'
 class Book < Item
-    attr_accessor :publisher, :cover_state
+  attr_accessor :publisher, :cover_state
 
-    def initialize(genre, author, label, date, publisher, cover_state, archived: false)
-        super(genre, author, label, date, archived)
-        @publisher = publisher
-        @cover_state = cover_state
-    end
+  def initialize(genre:, author:, label:, date:, publisher:, cover_state:, archived: false)
+    super(genre, author, label, date, archived)
+    @publisher = publisher
+    @cover_state = cover_state
+  end
 
-    def can_be_archived?
-        super || cover_state == 'bad'
-    end
+  def can_be_archived?
+    super || cover_state == 'bad'
+  end
 end
