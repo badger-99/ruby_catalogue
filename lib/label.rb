81e9b0e6b922663826e7d2304431c31a7f852e89
Haider <malikhaiderkhan81@gmail.com>
2023-10-03 19:37:02 +0500

    add initialize method

--- lib/label.rb
+++ lib/label.rb
@@ -1,1 +1,12 @@
 require_relative 'item'
+
+class label
+    attr_accessor :title, :color
+    attr_reader :id, :items
+
+    def initialize(title, color)
+        @id = Random.rand(1..1000)
+        @title = title
+        @color = color
+    end
+end
