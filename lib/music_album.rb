3f588c67618c513b2e8e45c5e23251762acd1b6b
Alfred M <alfred.mkg@proton.me>
2023-10-03 14:49:15 +0300

    Update music_album.rb initializer

--- lib/music_album.rb
+++ lib/music_album.rb
@@ -1,10 +1,12 @@
 require_relative 'item'
 
 class MusicAlbum < Item
-  def initialize(title, artist, _genre, spotify: true)
-    super()
+  def initialize(date, title, artist, genre, on_spotify: true)
+    super(date)
     @title = title
     @artist = artist
-    @on_spotify = spotify
+    @on_spotify = on_spotify
+    add_genre(genre)
+  end
   end
 end
