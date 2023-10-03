43ecb672033efd9cd8024c9d0edbf29f6ac7cb40
Haider <malikhaiderkhan81@gmail.com>
2023-10-03 20:00:53 +0500

    fix offenses

--- lib/label.rb
+++ lib/label.rb
@@ -1,18 +1,18 @@
 require_relative 'item'
 
 class Label
-    attr_accessor :title, :color
-    attr_reader :id, :items
+  attr_accessor :title, :color
+  attr_reader :id, :items
 
-    def initialize(title, color)
-        @id = Random.rand(1..1000)
-        @title = title
-        @color = color
-        @items = []
-    end
+  def initialize(title, color)
+    @id = Random.rand(1..1000)
+    @title = title
+    @color = color
+    @items = []
+  end
 
-    def add_item(item)
-        @items << item
-        item.label = self
-    end
+  def add_item(item)
+    @items << item
+    item.label = self
+  end
 end
