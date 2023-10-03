f2342e4a3fdd63c15a681767ce37a07c8df89ec0
Alfred M <alfred.mkg@proton.me>
2023-10-03 23:27:27 +0300

    Add GenreManager class to genre.rb

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,7 +1,7 @@
 class Genre
   attr_reader :name, :items
 
-  def initialize(name)
+  def initialize(name='Unknown')
     @id = Random.rand(1..1000)
     @name = name
     @items = []
@@ -10,5 +10,39 @@ class Genre
   def add_items(item)
     @items.push(item) unless @items.include?(item)
     item.add_genre(self)
+  end
+end
+
+class GenreManager
+  attr_reader :genre_list
+
+  def initialize
+    @genre_list = []
+  end
+
+  def create_genre
+    puts 'Enter the genre (press ENTER if you don\'t know it): '
+    name = gets.chomp.capitalize
+    genre = nil
+
+    if genre.empty?
+      genre = Genre.new()
+    else
+      genre = Genre.new(name)
+    end
+    genre
+  end
+
+  def add_genre(genre)
+    return unless genre.instance_of?(Genre)
+
+    match = @genre_list.select{ |item| item.name == genre.name }
+    @genre_list.push(genre) if match.empty?
+  end
+
+  def show_genres(genre_array)
+    genre_array.each_index do |genre, index|
+      puts "#{index+1}. #{genre.name}"
+    end
   end
 end
