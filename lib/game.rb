66a10d02ac5566c21a35116c656760c31dbf665a
devendra-alt <devendramulewa171@gmail.com>
2023-10-03 11:16:05 +0530

    Add can_be_achived? poly method

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,17 @@
+require_relative 'item'
+
+class Game < Item
+  attr_accessor :multiplayer, :last_played_at
+
   def initialize(genre, author, label, publish_date, archived, last_played_at, multiplayer: false)
     @last_played_at = Date.strptime(last_played_at)
     @multiplayer = multiplayer
     super(genre, author, label, publish_date, archived)
   end
+
+  private
+
+  def can_be_archived?
+    super && (Date.today - last_played_at > 2)
+  end
+end
