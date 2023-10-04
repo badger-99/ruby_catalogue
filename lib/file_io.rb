80222a0972fed671b81c1d744dbc38f1eb16c736
devendra-alt <devendramulewa171@gmail.com>
2023-10-04 22:03:12 +0530

    Add FileIO class in app.rb

---
+++ lib/file_io.rb
@@ -0,0 +1,15 @@
+require 'json'
+
+class FileIO
+  def file_exist?(file_name)
+    File.exist?(file_name) && File.size(file_name).positive?
+  end
+
+  def from_json(file)
+    JSON.parse(file)
+  end
+
+  def to_json(data)
+    data.to_json
+  end
+end
