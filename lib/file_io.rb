5fd882a428315a6a9a688ea7eede0e7ca215b122
devendra-alt <devendramulewa171@gmail.com>
2023-10-04 22:46:45 +0530

    Fix linter issues

--- lib/file_io.rb
+++ lib/file_io.rb
@@ -3,4 +3,12 @@ class FileIO
   def file_exist?(file_name)
     File.exist?(file_name) && File.size(file_name).positive?
   end
+
+  def from_json(file)
+    JSON.parse(file)
+  end
+
+  def to_json(data)
+    data.to_json
+  end
 end
