a0143eeb581d2d5b908cd554498b978441b8fa26
Alfred M <alfred.mkg@proton.me>
2023-10-04 19:34:58 +0300

    Add `to_hash` method to genre.rb & music_album.rb

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,5 +1,6 @@
 class Genre
   attr_reader :name, :items
+  attr_accessor :id
 
   def initialize(name = 'Unknown')
     @id = Random.rand(1..1000)
@@ -10,5 +11,12 @@ class Genre
   def add_items(item)
     @items.push(item) unless @items.include?(item)
     item.add_genre(self)
+  end
+
+  def to_hash
+    {
+      'id' => @id,
+      'name' => @name
+    }
   end
 end
--- lib/music_album.rb
+++ lib/music_album.rb
@@ -3,7 +3,7 @@ require_relative 'genre'
 
 class MusicAlbum < Item
   attr_reader :date, :title, :artist, :genre
-  attr_accessor :on_spotify
+  attr_accessor :id, :on_spotify
 
   def initialize(date, title, artist, genre, on_spotify)
     super(date)
@@ -15,5 +15,16 @@ class MusicAlbum < Item
 
   def move_to_archive
     @archived = can_be_archived? && @on_spotify
+  end
+
+  def to_hash
+    {
+      'id' => @id,
+      'date' => @publish_date,
+      'title' => @title,
+      'artist' => @artist,
+      'genre_name' => @genre.name,
+      'on_spotify' => @on_spotify
+    }
   end
 end
