31fc33b544b1750a60db865a081fd36640a7e56f
devendra-alt <devendramulewa171@gmail.com>
2023-10-05 16:07:03 +0530

    Add game IO method to file_io.rb

--- lib/file_io.rb
+++ lib/file_io.rb
@@ -11,4 +11,24 @@ class FileIO
   def to_json(data)
     data.to_json
   end
+
+  def save_author_data
+    author_data = []
+    File.open('author.json', 'w') do |file|
+      @authors.each do |author|
+        author_data << author.to_hash
+      end
+      file.write(author_data.to_json)
+    end
+  end
+
+  def save_game_data
+    game_data = []
+    File.open('game.json', 'w') do |file|
+      @games.each do |game|
+        game_data << game.to_hash
+      end
+      file.write(game_data.to_json)
+    end
+  end
 end
